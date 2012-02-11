855c8b6901022651a67f33a12a19a68c5c764090
Edoxile <koen_75_@hotmail.com>
2012-02-11 01:56:10 +0100

    Removed 'old' parts, CP'd some InventoryManager stuff, still have to fix it so it'll work like expected.

--- src/net/edoxile/bettermechanics/utils/InventoryManager.java
+++ src/net/edoxile/bettermechanics/utils/InventoryManager.java
@@ -18,10 +18,54 @@
 
 package net.edoxile.bettermechanics.utils;
 
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+
 /**
  * Created by IntelliJ IDEA.
  *
  * @author Edoxile
  */
 public class InventoryManager {
+    public static ItemStack[] putContents(Inventory iTo, ItemStack... sFrom) {
+        ItemStack[] sTo = iTo.getContents();
+        fromLoop:
+        for (int i = 0; i < sFrom.length; i++) {
+            ItemStack fStack = sFrom[i];
+            if (fStack == null) {
+                continue;
+            } else {
+                toLoop:
+                for (int j = 0; j < sTo.length; j++) {
+                    ItemStack tStack = sTo[j];
+                    if (tStack == null) {
+                        sTo[j] = fStack;
+                        sFrom[i] = null;
+                        continue fromLoop;
+                    } else if (fStack.getTypeId() == tStack.getTypeId() && fStack.getDurability() == tStack.getDurability() && tStack.getEnchantments().isEmpty()) {
+                        int total = fStack.getAmount() + tStack.getAmount();
+                        if (total > 64) {
+                            tStack.setAmount(64);
+                            fStack.setAmount(total - 64);
+                        } else {
+                            tStack.setAmount(total);
+                            int remainder = total - 64;
+                            if (remainder == 0) {
+                                sFrom[i] = null;
+                                sTo[j] = tStack;
+                                continue fromLoop;
+                            } else {
+                                fStack.setAmount(remainder);
+                            }
+                        }
+                    } else {
+                        continue;
+                    }
+                    sTo[j] = tStack;
+                }
+            }
+            sFrom[i] = fStack;
+        }
+        return sFrom;
+    }
 }
